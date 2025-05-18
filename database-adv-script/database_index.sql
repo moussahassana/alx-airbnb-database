6cdd4b87cc4d21c899bd179a75342562c6986612
Moussa Hassana <moussahassana42@gmail.com>
2025-05-18 10:52:11 +0100

    chore(index): include EXPLAIN ANALYZE before and after indexing as required

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,5 +1,14 @@
--- Index for faster lookups by status in Booking
+-- Before index
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE status_id = '7a29b8b4-9a2e-44a3-b7b3-359404e1b15f';
+
+-- Create index for status_id
 CREATE INDEX idx_booking_status ON Booking(status_id);
+
+-- After index
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE status_id = '7a29b8b4-9a2e-44a3-b7b3-359404e1b15f';
+
 
 -- Index on Booking start_date for partitioning and range queries
 CREATE INDEX idx_booking_start_date ON Booking(start_date);
@@ -12,3 +21,6 @@ CREATE INDEX idx_review_rating ON Review(rating);
 
 -- Index on Payment method
 CREATE INDEX idx_payment_method ON Payment(method_id);
+
+
+--
