d6fb2c4040f83c395702129a43ff61babf234b6a
Moussa Hassana <moussahassana42@gmail.com>
2025-05-18 11:36:46 +0100

    perf: add EXPLAIN ANALYZE query with filters to performance.sql for booking details

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,4 +1,4 @@
--- Initial complex query joining Bookings, Users, Property, and Payment details
+EXPLAIN ANALYZE
 SELECT
     b.booking_id,
     b.start_date,
@@ -16,4 +16,6 @@ SELECT
 FROM Booking b
 JOIN users u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pm ON b.booking_id = pm.booking_id;
+LEFT JOIN Payment pm ON b.booking_id = pm.booking_id
+WHERE b.status_id = '7a29b8b4-9a2e-44a3-b7b3-359404e1b15f'
+  AND b.start_date >= '2025-01-01';
