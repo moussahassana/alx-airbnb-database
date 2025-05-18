dfe2014d24afad997235ec1a32284eaa13573102
Moussa Hassana <moussahassana42@gmail.com>
2025-05-18 10:23:20 +0100

    feat(window-functions): rank properties by booking count using RANK

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -4,3 +4,11 @@ FROM users u
 JOIN Booking b ON u.user_id = b.user_id
 GROUP BY u.user_id, u.first_name, u.last_name
 ORDER BY total_bookings DESC;
+
+-- Rank properties based on number of bookings
+SELECT p.property_id, p.name, COUNT(b.booking_id) AS total_bookings,
+       RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_rank
+FROM Property p
+LEFT JOIN Booking b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name
+ORDER BY property_rank;
